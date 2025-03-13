71cc75583f84db7215e1d5ec15e0b527e4ec6744
Diana Zinevych <dianazinevych02@gmail.com>
2025-03-13 15:44:20 +0100

    done all tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ContactList from "./components/ContactList/ContactList";
 import initialContacts from "./contacts.json";
 import "./App.css";
@@ -6,17 +6,36 @@ import SearchBox from "./components/SearchBox/SearchBox";
 import ContactForm from "./components/ContactForm/ContactForm";
 
 export default function App() {
-  const [contacts, setContacts] = useState(initialContacts);
+  //Стан зберігається у памяті вкладці браузера
+  const [contacts, setContacts] = useState(() => {
+    // Зчитуємо значення за ключем
+    const savedContacts = localStorage.getItem("contacts");
+    // якщо там щось є, парсимо та повертаємо значення стану або повертаємо значення за замовч
+    return savedContacts ? JSON.parse(savedContacts) : initialContacts;
+  });
+
+// Зберігаємо в локальне сховище контакти між перезавантаженнями сторінки, при умові якщо масив не порожній (Це зроблено, щоб уникнути збереження порожнього масиву при першому завантаженні)
+  useEffect(()=> {
+    if (contacts.length > 0) {
+    localStorage.setItem("contacts", JSON.stringify(contacts));
+  }
+  // Ефект буде виконуватись вище кожного разу тільки тоді, коли змінюється стан контакс
+ }, [contacts]);
+
   const [search, setSearch] = useState("");
-
-  // const addContacts = (newContacts) => {
-  //   setContacts((prevContacts) => {
-  //     return [...prevContacts, newContacts];
-  //   });
-  // };
-
+//Функція зміни стану
+  const addContacts = (newContacts) => {
+    //Буде викликана реактом під капотом і отримає обовґязковий один аргумент, обявляємо параметр, що буде поточне значення стану contacts на момент оновлення
+    setContacts((prevContacts) => {
+    //Розпилюємо попередні контакти і додаємо нові контакти
+      return [...prevContacts, newContacts];
+    });
+  };
+//
   const deleteContacts = (contactId) => {
+    //Використовуємо функіональну форму сетеру, повертає новий стан,
     setContacts(prevContacts => {
+      //Відфільтрує масив, якщо індентифікатор співпаде це означатиме що той елемент, котрий потрібно видалити і не піде в новий масив
       return prevContacts.filter((contact) => contact.id !== contactId);
     });
   };
@@ -26,7 +45,7 @@ export default function App() {
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm onAdd={addContacts}/>
       <SearchBox value={search} onSearch={setSearch}/>
       <ContactList contacts={visibleContacts} onDelete={deleteContacts}/>
     </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,31 +1,47 @@
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from "yup";
-import { ErrorMessage } from "formik";
 import css from "./ ContactForm.module.css";
 import { nanoid } from 'nanoid';
 import { useId } from 'react';
+const initialValues = {
+    name: "", 
+    number: ""
+  };
+  const ContactSchema = Yup.object().shape({
+    name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required"),
+    number: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required")
+});
 
-const ContactForm = () => {
-    const FeedBackSchema = Yup.object().shape({name: Yup.string().min(3, "Too Short!").max(50, "Too Long!").required("Required")});
+const ContactForm = ({onAdd}) => {
+    
     const fieldId = useId();
     const handleSubmit= (values, actions) => {
         actions.resetForm();
+        onAdd({
+            id: nanoid(),
+            name: values.name,
+            number: values.number,
+        })
     };
     return (
-        <Formik initialValues={{name: "", number: ""}}
+        <Formik 
+        initialValues={initialValues}
         onSubmit={handleSubmit}
+        validationSchema={ContactSchema}
         >
             <Form className={css.form}>
                 <div className={css.group}>
                     <label htmlFor={'${useId}-name'}>Name</label>
                     <Field className={css.input} type="text" name="name" id={'${useId}-name'}/>
+                    <ErrorMessage className={css.error} name="name" component="span"/>
                 </div>
 
                 <div className={css.group}>
                     <label htmlFor={'${useId}-number'}>Number</label>
                     <Field className={css.input} type="number" name="number" id={'${useId}-number'}/>
+                    <ErrorMessage className={css.error} name="number" component="span"/>
                 </div>
-                <button type="submit">Add contact</button>
+                <button className={css.btnadd} type="submit">Add contact</button>
             </Form>
 
         </Formik>
